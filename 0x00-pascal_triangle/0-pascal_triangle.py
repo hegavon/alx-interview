69cb3cea7faeb5763f7a3201264a6f337e3d9008
Victor Amajuoyi <victorgavon@gmail.com>
2024-06-27 17:25:56 +0100

    Pascals Triangle Task done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,10 +17,17 @@ def pascal_triangle(n):
 
         for j in range(1, i):
             if j < len(new_row):
-                a = pascal_triangle[i - 1][j] if j < len(pascal_triangle[i - 1]) else 0
-                b = pascal_triangle[i - 1][j - 1] if j - 1 >= 0 else 0
+                a = get_value_or_zero(pascal_triangle[i - 1], j)
+                b = get_value_or_zero(pascal_triangle[i - 1], j - 1)
                 new_row[j] = a + b
 
         pascal_triangle.append(new_row)
 
     return pascal_triangle
+
+
+def get_value_or_zero(row, index):
+    if index < len(row):
+        return row[index]
+    else:
+        return 0
