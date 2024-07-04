7315d66c4c00384ecbfa4989fb1a30c65cbb9509
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-04 20:05:21 +0100

    Alx Interview - Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 '''LockBoxes Challenge'''
-from typing import List
+
+from collections import deque
 
 
-def canUnlockAll(boxes: List[List[int]]) -> bool:
-    if not boxes or len(boxes) == 0:
-        return False
+def canUnlockAll(boxes):
+    '''Determines if all the boxes can be opened or not.
 
+    Args:
+        boxes (list of lists): Each sublist contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    '''
     n = len(boxes)
     visited = set()
-    to_visit = [0]  # Start with box 0 (the first box)
+    to_visit = deque([0])  # Start with box 0 (the first box)
 
     while to_visit:
-        current_box = to_visit.pop(0)
+        current_box = to_visit.popleft()
         visited.add(current_box)
         for key in boxes[current_box]:
             if key < n and key not in visited:
