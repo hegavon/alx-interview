072dc05b436f50ca593ca3484371bc7a268714d1
Victor Amajuoyi <victorgavon@gmail.com>
2024-08-09 01:16:15 +0100

    Star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require('request');
+import request from 'request';
 
 const movieId = process.argv[2];
 const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
