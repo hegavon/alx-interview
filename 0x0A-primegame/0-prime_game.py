43a1ac007a703f47e446cae284dc42b866366c1c
Victor Amajuoyi <victorgavon@gmail.com>
2024-09-05 12:10:59 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,81 @@
 #!/usr/bin/python3
+'''Prime Game'''
+
 
 def sieve_of_eratosthenes(n):
-    """ Return a list of prime numbers up to n """
+    '''Generates all prime numbers up to n using the Sieve of Eratosthenes'''
     sieve = [True] * (n + 1)
     sieve[0] = sieve[1] = False
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if sieve[i]:
-            for j in range(i*i, n + 1, i):
+            for j in range(i * i, n + 1, i):
                 sieve[j] = False
-    return [i for i in range(2, n + 1) if sieve[i]]
+    return sieve
 
 
 def isWinner(x, nums):
-    """ Determine who the winner is after x rounds of prime game """
-    if not nums or x <= 0:
+    '''finds the winner of the prime game'''
+    if not nums or x < 1:
         return None
 
-    # Calculate the maximum number in nums
+    # Find the maximum number in nums to precompute primes up to that number
     max_num = max(nums)
 
-    # Precompute prime numbers up to max_num using Sieve of Eratosthenes
-    primes_up_to_max = sieve_of_eratosthenes(max_num)
+    # Precompute primes using the Sieve of Eratosthenes
+    primes = sieve_of_eratosthenes(max_num)
 
-    # Track wins
-    maria_wins = 0
-    ben_wins = 0
+    # To track the winner count for both Maria and Ben
+    winnerCounter = {'Maria': 0, 'Ben': 0}
 
+    # Simulate each round
     for n in nums:
-        primes = [p for p in primes_up_to_max if p <= n]
-        turn = 0  # 0 for Maria, 1 for Ben
+        round_winner = isRoundWinner(n, primes)
+        if round_winner is not None:
+            winnerCounter[round_winner] += 1
 
-        while primes:
-            prime = primes.pop(0)
-            primes = [p for p in primes if p % prime != 0]
-            turn = 1 - turn  # Switch turns
-
-        if turn == 0:
-            ben_wins += 1  # Ben won
-        else:
-            maria_wins += 1  # Maria won
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
+    # Determine the overall winner
+    if winnerCounter['Maria'] > winnerCounter['Ben']:
+        return 'Maria'
+    elif winnerCounter['Ben'] > winnerCounter['Maria']:
+        return 'Ben'
     else:
         return None
+
+
+def isRoundWinner(n, primes):
+    '''Simulates the game for one round and returns the round winner'''
+    list_of_nums = [i for i in range(1, n + 1)]
+    players = ['Maria', 'Ben']
+    currentPlayerIndex = 0
+
+    while True:
+        prime = -1
+        selected_idxs = []
+
+        # Current player
+        currentPlayer = players[currentPlayerIndex % 2]
+
+        for idx, num in enumerate(list_of_nums):
+            if prime != -1:
+                if num % prime == 0:
+                    selected_idxs.append(idx)
+            else:
+                if primes[num]:  # Check if number is prime
+                    selected_idxs.append(idx)
+                    prime = num
+
+        # If no prime number was selected, the current player loses
+        if prime == -1:
+            return players[(currentPlayerIndex + 1) % 2]
+        else:
+            # Remove selected prime and its multiples from the list
+            for i, idx in enumerate(selected_idxs):
+                del list_of_nums[idx - i]
+
+        # Switch to the next player
+        currentPlayerIndex += 1
+
+
+# Example for testing:
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
